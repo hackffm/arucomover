90fc84006cca3dfe584a2814812c0e71249fd0c4
vkuehn <vkuehn@more-machines.org>
2021-07-03 15:18:34 +0200

    remove obsolete stuff

--- code/bash/m_4_get_files.sh
+++ code/bash/m_4_get_files.sh
@@ -1,2 +1,2 @@
 echo "backup m_4 files make sure it's mounted !"
-rsync -rtuvP --human-readable --modify-window=1 --stats --exclude-from ./syncexclude.txt /media/pi/circuitpython/ ~/git/playground/mmMover2020_03/code/m_4
+rsync -rtuvP --human-readable --modify-window=1 --stats --exclude-from ./syncexclude.txt /media/pi/circuitpython/ ../m_4
