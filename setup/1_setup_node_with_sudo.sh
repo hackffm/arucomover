091c06f56ef992149ac65428a6664d7aa7f681b8
vkuehn <vkuehn@more-machines.org>
2022-04-15 16:35:25 +0200

    set node version to lts

--- setup/1_setup_node_with_sudo.sh
+++ setup/1_setup_node_with_sudo.sh
@@ -1,3 +1,3 @@
-apt install curl
-curl -fsSL https://deb.nodesource.com/setup_17.x | bash -
+apt install curl gcc g++ make
+curl -fsSL https://deb.nodesource.com/setup_16.x | bash -
 apt install -y nodejs
